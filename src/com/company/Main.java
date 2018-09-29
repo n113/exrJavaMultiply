f5890556731eaae6606cb2fb7494f3aabffb3877
n113 <rfedsxc@mail.ru>
2018-09-29 03:05:58 -0700

    small changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,7 +20,6 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-    ArrayList<Integer> numbers = new ArrayList<>();
     Random rand = new Random();
     ArrayList<String> checking = new ArrayList<>();
 
